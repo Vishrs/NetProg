1cdedc99d46f696bb2e6cd3d694591b6d00d221d
vishrs1 <utnip123@yahoo.com>
2016-03-08 23:30:06 +0000

    Modularize

--- vemulv_hw1.c
+++ vemulv_hw1.c
@@ -9,6 +9,29 @@
 #include <arpa/inet.h>
 #define PORT "12345"
 
+void sendMessageUDP(char *id, int sd, char *servaddr) {
+    struct addrinfo adrinfo;
+    struct addrinfo *adrresults;
+    
+    memset(&adrinfo, 0, sizeof(adrinfo));
+    adrinfo.ai_family = AF_INET6;
+    adrinfo.ai_socktype = SOCK_DGRAM;
+    adrinfo.ai_flags = AI_V4MAPPED | AI_ADDRCONFIG;
+    
+    int rend_info = getaddrinfo(servaddr, PORT, &adrinfo, &adrresults);
+    
+    if (rend_info < 0) {
+        perror("getaddrinfo() failed\n");
+        return;
+    }
+    
+    int snd = sendto(sd, id, strlen(id), 0, adrresults->ai_addr, adrresults->ai_addrlen);
+    if (snd < 0) {
+        printf("ERROR: sendto() failed\n");
+        return;
+    }
+}
+
 int main(int argc, char * argv[]) {
     
     if (argc < 5) {
@@ -18,46 +41,27 @@ int main(int argc, char * argv[]) {
     }
     
     char ownPeerID[80];
-    strcpy(ownPeerID, argv[2]); //char ownPeerID[80] = argv[2];
-   // ownPeerID[strlen(ownPeerID)] = '\0';
+    strcpy(ownPeerID, argv[2]);
     
     char registerID[80] = "REGISTER ";
     
     strcat(registerID, ownPeerID);
     registerID[strlen(registerID)] = '\0';
-    printf("%s\n", registerID);
-    struct sockaddr_in server;
-    struct addrinfo adrinfo;
-    struct addrinfo *adrresults;
-    
+
     int sd;
-    
     if((sd = socket( AF_INET6, SOCK_DGRAM, 0)) < 0) {
         perror("socket failed\n");
         return EXIT_FAILURE;
     }
+    sendMessageUDP(registerID, sd, argv[1]);
+    //server.sin_family = AF_INET;
+    //server.sin_addr.s_addr = htonl(INADDR_ANY);
+    //server.sin_port = htons(12345);
+    char peerID[80];
+    char message[80];
+    strcpy(peerID, argv[3]);
+    strcpy(message, argv[4];)
     
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
-    server.sin_port = htons(12345);
-    
-    memset(&adrinfo, 0, sizeof(adrinfo));
-    adrinfo.ai_family = AF_INET6;
-    adrinfo.ai_socktype = SOCK_DGRAM;
-    adrinfo.ai_flags = AI_V4MAPPED | AI_ADDRCONFIG;
-    
-    int rend_info = getaddrinfo(argv[1], PORT, &adrinfo, &adrresults);
-    
-    if (rend_info < 0) {
-        perror("getaddrinfo() failed\n");
-        return EXIT_FAILURE;
-    }
-    
-    int snd = sendto(sd, registerID, strlen(registerID), 0, adrresults->ai_addr, adrresults->ai_addrlen);
-    if (snd < 0) {
-        printf("ERROR: sendto() failed\n");
-        return EXIT_FAILURE;
-    }
     
     return EXIT_SUCCESS;
     
