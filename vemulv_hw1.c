6bee3a8179c52089bc594795c3e20a2a665e156b
vishrs1 <utnip123@yahoo.com>
2016-03-08 02:18:34 +0000

    initial addr info

--- vemulv_hw1.c
+++ vemulv_hw1.c
@@ -16,6 +16,34 @@ int main(int argc, char * argv[]) {
         return EXIT_FAILURE;
     }
     
+    struct sockaddr_in server;
+    struct addrinfo adrinfo;
+    struct addrinfo adrresults;
+    
+    int sd, length;
+    
+    if((sd = socket( AF_INET6, SOCK_DGRAM) < 0) {
+        perror("socket failed\n");
+        return EXIT_FAILURE;
+    }
+    
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons(12345);
+    
+    memset(&adrinfo, 0, sizeof(adrinfo));
+    adrinfo.ai_family = AF_INET6;
+    adrinfo.ai_socktype = SOCK_DGRAM;
+    adrinfo.ai_flags = AI_V4MAPPED | AI_ADDRCONFIG;
+    
+    int rend_info = getaddrinfo(argv[1], "12345", &adrinfo, &adrresults);
+    
+    if (rend_info < 0) {
+        perror("getaddrinfo() failed\n");
+        return EXIT_FAILURE;
+    }
+    
+    
     return EXIT_SUCCESS;
     
 }
