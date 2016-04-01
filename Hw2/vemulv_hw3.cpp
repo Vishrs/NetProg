571cc443cbb28a51d14d494b0d425b810699e4f3
vishrs1 <utnip123@yahoo.com>
2016-04-01 03:25:35 +0000

    connects

--- Hw2/vemulv_hw3.cpp
+++ Hw2/vemulv_hw3.cpp
@@ -56,6 +56,37 @@ void parse_url(string &url, string &host, string &port, string &path) {
     }
 }
 
+connect_to_server(int &sockfd, char* host, char* port) {
+    struct addrinfo ai_hints;
+    struct addrinfo* ai_results;
+    
+    memset(&ai_hints, 0, sizeof(ai_hints));
+	ai_hints.ai_family = AF_UNSPEC;
+	ai_hints.ai_socktype = SOCK_STREAM;
+	ai_hints.ai_flags = AI_V4MAPPED | AI_ADDRCONFIG;   
+    if((sockfd = socket(AF_INET6, SOCK_STREAM, 0)) < 0) {
+        perror("sockfd()");
+        return;
+    }
+    int ret = getaddrinfo(host, port, &ai_hints, &ai_results);
+    if (ret < 0) {
+        perror("getaddrinfo()");
+        return;
+    }
+    
+    bool connected = false;
+    int i;
+	for (i = ai_results;i != NULL;i = j->ai_next) {
+		ret = connect(sockfd, i->ai_addr, i->ai_addrlen);
+		if (ret == 0) {
+			conntected = true;
+			break;
+		}
+	}
+    
+    
+}
+
 int main(int argc, char* argv[]) {
     
     if (argc != 2) {
@@ -65,7 +96,14 @@ int main(int argc, char* argv[]) {
     
     string input = argv[1];
     string host, port, path;
+    char* c_host = host.c_str();
+    char* c_port = port.c_str();
+
     parse_url(input, host, port, path);
     //std::cout << input << " " << host << " " << port << " " << path << std::endl;
+    int sockfd;
+    connect_to_server(sockfd, c_host, c_port);
+    
+    
     return EXIT_SUCCESS;
 }
