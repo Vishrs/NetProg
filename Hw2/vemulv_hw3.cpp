cd6b899b00fe4155b4ba0b38db20ae54c7c5beb9
vishrs1 <utnip123@yahoo.com>
2016-04-01 02:28:15 +0000

    parsing finished

--- Hw2/vemulv_hw3.cpp
+++ Hw2/vemulv_hw3.cpp
@@ -13,6 +13,7 @@
 using std::string;
 #define PORT 80
 
+//=====================================Helper functions===============================
 bool icompare_pred(unsigned char a, unsigned char b)
 {
     return std::tolower(a) == std::tolower(b);
@@ -28,10 +29,27 @@ bool icompare(std::string const& a, std::string const& b)
         return false;
     }
 }
+//===================================================================================
+
 
 void parse_url(string &url, string &host, string &port, string &path) {
-    if (icompare(url.substr(0,4), "http"))
-        int host_ind = url.find(":", 5);
+    if (icompare(url.substr(0,4), "http")) {
+        int port_ind = url.find(":", 5);
+        int path_ind = url.find("/", 7);
+        bool path_exists = true;
+        if (path_ind == string::npos) {
+            path_ind = url.size();
+        }
+        if (port_ind == string::npos) {
+            port = "80";
+            port_ind = path_ind;
+        } else {
+            port = url.substr(port_ind + 1, path_ind - port_ind - 1);
+        }
+        
+        host = url.substr(7, port_ind - 7);
+        path = url.substr(path_ind, url.size() - path_ind);
+    }
     else {
         printf("Invalid url, include http://\n");
         return;
@@ -48,6 +66,6 @@ int main(int argc, char* argv[]) {
     string input = argv[1];
     string host, port, path;
     parse_url(input, host, port, path);
-    
+    //std::cout << input << " " << host << " " << port << " " << path << std::endl;
     return EXIT_SUCCESS;
 }
